46db9212c654e30d6de2424d3980104b1b567340
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-30 00:05:33 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE crowdfunding (id INTEGER PRIMARY KEY, project TEXT, users TEXT, pledges INTEGER);
+"CREATE TABLE crowdfunding (id INTEGER PRIMARY KEY, project TEXT, users TEXT, pledges INTEGER);""
